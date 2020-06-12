8b8c4cedee90ec531880caf7238cd35aee3b507a
muhwezideo <aggrey256@gmail.com>
2020-06-12 15:49:38 +0300

    setup project

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (range(0, 10) as $number) {
+            $rand = rand(0, 1);
+            dump($rand);
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('password'),
+                'is_candidate'=> $rand == 0 ? false : true,
+                'created_at'=>\Carbon\Carbon::now(),
+                'updated_at' =>\Carbon\Carbon::now()
+            ]);
+        }
+    }
+}
